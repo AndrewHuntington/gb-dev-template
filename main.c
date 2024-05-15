9cee710c7ce99d1856ae5912462143594aa9b8af
Andrew Huntington <andrew.huntington@gmail.com>
2024-05-16 08:15:12 +0900

    add boilerplate settings

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
+// To  build: shift + cmd + b (mac) or shift + ctrl + b (win)
+// To build, save and debug: F5 (with Emulicious running)
+
 #include <gb/gb.h>
+#include <stdio.h>
+
+void main()
+{
+    printf("Hello World!");
+}
